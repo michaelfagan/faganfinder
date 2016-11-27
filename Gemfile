8ae4c20db3696d706bcbb94416d8b8bacf06cd94
Michael Fagan <mfagan@gmail.com>
2016-11-26 22:17:12 -0500

    adding stylesheet, with sass

--- Gemfile
+++ Gemfile
@@ -1,6 +1,10 @@
 source 'https://rubygems.org'
 
 gem 'erubis'
+
 gem 'json'
+
 gem 'html5_validator' # https://github.com/damian/html5_validator
 gem 'htmlcompressor' # https://github.com/paolochiodi/htmlcompressor
+
+gem 'sass'
--- index.rb
+++ index.rb
@@ -2,21 +2,31 @@ require 'erubis'
 require 'json'
 require 'htmlcompressor'
 require 'html5_validator/validator'
+require 'sass'
 
-# data for the template
+# compile css
+begin
+  css = Sass::Engine.new(File.read('index.scss'), syntax: :scss, style: :compressed).render
+rescue Exception => e
+  STDERR.puts "SCSS error: #{e.message}"
+  exit
+end
+
+# other data for the template
 engines = JSON.parse File.read('index.json'), object_class: OpenStruct
 
 # generate html from the template
 html = Erubis::Eruby.new(File.read 'index.erb.html').result(binding())
 
 # output pretty html for readability
-File.write 'temp/index.html', html
+html_path = 'temp/index.html'
+File.write html_path, html
 
 # validate the html
 html_errors = Html5Validator::Validator.new.validate_text html
 if html_errors.any?
   html_errors.map!{|e| "line #{e['lastLine']} #{e['message']}\n\t" + e['extract'].sub(/\n/, "\n\t") }
-  html_errors.unshift 'HTML validation errors'
+  html_errors.unshift "HTML validation errors - see #{html_path}"
   STDERR.puts html_errors.join "\n\n"
   exit
 end
