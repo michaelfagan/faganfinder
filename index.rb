9481a77abbc7d40e48c3dd30312873db41db5d50
Michael Fagan <mfagan@gmail.com>
2016-11-26 20:20:31 -0500

    renaming html file, fixing indentation

--- index.rb
+++ index.rb
@@ -3,6 +3,6 @@ require 'json'
 
 engines = JSON.parse File.read('index.json'), object_class: OpenStruct
 
-html = Erubis::Eruby.new(File.read 'index.eruby').result(binding())
+html = Erubis::Eruby.new(File.read 'index.erb.html').result(binding())
 
 File.write 'dist/index.html', html
