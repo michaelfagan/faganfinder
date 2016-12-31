ef7ede29eecb76597797756da5fbd34271529a77
Michael Fagan <mfagan@gmail.com>
2016-12-30 21:01:48 -0500

    adding ability to use POST and two video search tools which do so. minor js refactoring, minor reordering of video search sections

--- index.js
+++ index.js
@@ -6,24 +6,46 @@ body.classList.add('has-js');
 
 var form = document.forms[0];
 
+// add the form needed for tools that use POST
+form.insertAdjacentHTML('afterend', '<form target="_blank" style="display:none" action="" method="post"><input></form>');
+
+function setTool(button) {
+  var post = button.getAttribute('data-post');
+  form.setAttribute('data-post', post);
+  if (post) {
+    document.forms[1].setAttribute('action', button.getAttribute('value'));
+    document.forms[1].elements[0].setAttribute('name', post);
+  }
+  else {
+    form.setAttribute('data-s', button.getAttribute('value'));
+  }
+}
+
+// initialize
+setTool(form.elements[1]);
+
+// run the search
 form.addEventListener('submit', function(e) {
   var q = form.elements[0].value;
   if (q.length) {
-    location.href = form.getAttribute('data-s').replace('{s}', encodeURIComponent(q));
+    if (form.getAttribute('data-post') !== 'null') {
+      document.forms[1].elements[0].setAttribute('value', q);
+      document.forms[1].submit();
+    }
+    else {
+      open(form.getAttribute('data-s').replace('{s}', encodeURIComponent(q)));
+    }
   }
   e.preventDefault();
   return false;
 });
-form.setAttribute('data-s', form.querySelectorAll('ul button')[0].getAttribute('value')); // default
 
-function set_tool(button) {
-  form.setAttribute('data-s', button.getAttribute('value'));
-}
-
+// click on a button to search
 form.getElementsByTagName('ul')[0].addEventListener('click', function(e){
   if (e.target.tagName.toLowerCase() === 'button') {
-    set_tool(e.target);
+    setTool(e.target);
+  }
+  else if (e.target.parentNode.tagName.toLowerCase() === 'button') {
+    setTool(e.target.parentNode);
   }
 });
-
-set_tool(form.elements[2]);
